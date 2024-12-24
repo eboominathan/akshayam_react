03ee07342f251113d9460e687358a51ddbaf22b3
Boominathan <eboominathan@gmail.com>
2024-12-24 06:37:47 +0530

    Route created

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from './components/ui/button'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Welcome To Akshayam
+      <Outlet />
     </>
   )
 }
